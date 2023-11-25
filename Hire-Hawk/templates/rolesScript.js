d897227b6492dff8f78585568497424377d2a48d
ShiwamPaudel <106426216+ShiwamPaudel@users.noreply.github.com>
2023-11-25 23:45:47 +0545

    Files

---
+++ Hire-Hawk/templates/rolesScript.js
@@ -0,0 +1,9 @@
+/* Script for roles.html page */
+function redirectToRecruiterPage() {
+    window.location.href = 'Recruiter/RecLogin.html';
+  }
+  
+  function redirectToApplicantPage() {
+    window.location.href = 'Applicant/AppliDashboard.html';
+  }
+  
